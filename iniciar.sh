bb965782d9e349c52ab0e49a424e1508e1429d0b
brayanflores29925-ctrl <brayanflores29925@gmail.com>
2026-09-24 16:34:32 -0600

    Actualizar iniciar.sh

--- iniciar.sh
+++ iniciar.sh
@@ -1,12 +1,2 @@
 #!/data/data/com.termux/files/usr/bin/bash
-
-read -s -p "🔐 PIN de LeviBots: " PIN
-echo
-
-if [ "$PIN" != "kerin" ]; then
-  echo "❌ PIN incorrecto"
-  exit 1
-fi
-
-echo "✅ PIN correcto. Iniciando LeviBots..."
 node index.js
